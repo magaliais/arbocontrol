aab299e5515979dbe5b0cb1bb6353e2f3683706c
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-10-04 20:18:37 -0300

    👌 IMPROVE: new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from 'cors';
 import { CreateComplaintController } from "./use-cases/createComplaint/createComplaintController";
+import { GetComplaintsController } from "./use-cases/getComplaints/getComplaintsController";
+import { GetComplaintsStatusController } from "./use-cases/getComplaintsStatus/getComplaintsStatusController";
 
 const createComplaintController = new CreateComplaintController();
+const getComplaintsController = new GetComplaintsController();
+const getComplaintsStatusController = new GetComplaintsStatusController();
 
 export const routes = express.Router();
 
@@ -27,3 +31,49 @@ routes.post("/complaint", (req, res) => {
     return res.status(500).send();
   }
 });
+
+routes.get("/complaints", (req, res) => { 
+  console.log("⚡️ [server]: GET /complaints");
+
+  // * Permite que a URL de produção faça esta requisição
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  try {
+    const response = getComplaintsController.handle(req, res);
+    return response;
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+routes.get("/dashboard", (req, res) => { 
+  console.log("⚡️ [server]: GET /dashboard");
+
+  // * Permite que a URL de produção faça esta requisição
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  try {
+    const response = getComplaintsStatusController.handle(req, res);
+    return response;
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+// TODO rota para criar usuário
+// routes.post("/user", (req, res) => { 
+//   console.log("⚡️ [server]: POST /user");
+
+//   // * Permite que a URL de produção faça esta requisição
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+//   try {
+//     const response = createUserController.handle(req, res);
+//     return response;
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).send();
+//   }
+// });
