8bc9387fabb4c1ae0a56a25d3aa769c50f7ae5c9
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-10-06 22:50:05 -0300

    📦 NEW: mocked data for selects

---
+++ form/src/mocks/neighborhoods.ts
@@ -0,0 +1,25 @@
+const neighborhoods = [
+  { id: "215", name: "Adolpho Vireque" },
+  { id: "223", name: "Aeroporto" },
+  { id: "676", name: "Aeroporto Municipal Francisco Álvares de Assis" },
+  { id: "302", name: "Aldeia" },
+  { id: "498", name: "Alphaville" },
+  { id: "296", name: "Alta Vista" },
+  { id: "326", name: "Alto Bairú" },
+  { id: "646", name: "Alto da Serra" },
+  { id: "317", name: "Alto do Maracujá" },
+  { id: "260", name: "Alto Dom Bosco" },
+  { id: "181", name: "Alto dos Passos" },
+  { id: "465", name: "Alto dos Pinheiros" },
+  { id: "230", name: "Alto Grajaú" },
+  { id: "335", name: "Alto Jardim de Alá" },
+  { id: "457", name: "Alto Santa Luzia" },
+  { id: "327", name: "Alto São Geraldo" },
+  { id: "390", name: "Alto Sumaré" },
+  { id: "196", name: "Amazônia" },
+  { id: "3", name: "Angolinha" },
+  { id: "218", name: "Aracy" },
+  
+];
+
+export default neighborhoods;
