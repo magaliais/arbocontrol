7b5bcdab1c5fd4a0c811a35b8d6fa36fe73be056
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-12-22 19:13:32 -0300

    📦 NEW: protocol modal

--- form/src/pages/Home/index.tsx
+++ form/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './styles'
 
 import Hero from '../../components/Hero';
@@ -7,18 +7,32 @@ import FAQ from '../../components/FAQ';
 import Footer from '../../components/Footer';
 
 import Fade from 'react-reveal/Fade'
+import Modal from '../../components/Modal';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [complaintProtocol, setComplaintProtocol] = useState('12.05.22');
 
   async function handleSubmit() {}
 
+  isModalOpen
+    ? (document.getElementsByTagName("body")[0].style.overflowY = "hidden")
+    : (document.getElementsByTagName("body")[0].style.overflowY = "auto");
+
   return (
     <S.Container>
+      {isModalOpen && 
+        <Modal 
+          isModalOpen={isModalOpen} 
+          setIsModalOpen={setIsModalOpen} 
+          complaintProtocol={complaintProtocol}
+        />
+      }
 
       <Hero />
       
       <Fade bottom >
-        <Form />
+        <Form setIsModalOpen={setIsModalOpen} setComplaintProtocol={setComplaintProtocol}/>
       </Fade>
 
       <FAQ />
