53f57331c39ec8d7567b62106beeb8dc353adca1
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2023-02-08 12:05:07 -0300

    📦 NEW: mocks for status and neighborhoods

---
+++ dashboard/src/mocks/statusOptions.ts
@@ -0,0 +1,8 @@
+const statusOptions = [
+  { id: "pending", name: "Pendente" },
+  { id: "invalid", name: "Solicitação Inválida" },
+  { id: "inAttendance", name: "Em Atendimento" },
+  { id: "finished", name: "Concluído" },
+];
+
+export default statusOptions;
