be4f54a4496f5b9aa832b09f02a6abd8e25ebfd9
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2023-07-05 16:14:29 -0300

    🐛 FIX: deploy

--- server/src/prisma/client.ts
+++ server/src/prisma/client.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL
-    },
-  },
+      url: "postgres://10855854685:rDMB9fecj4iQ@ep-divine-river-801745.us-east-2.aws.neon.tech/arbocontrol?pgbouncer=true&connect_timeout=10",
+    },
+  },
 });
